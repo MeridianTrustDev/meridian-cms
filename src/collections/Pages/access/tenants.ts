f40c232d8a81cadaf662ed0f8950e98f6b4d252e
Rhysypops <developer@meridiantrust.co.uk>
2024-04-16 08:55:52 +0100

    Update tenants.ts

--- src/collections/Pages/access/tenants.ts
+++ src/collections/Pages/access/tenants.ts
@@ -1,27 +1,12 @@
 import type { Access } from 'payload/types'
 import { isSuperAdmin } from '../../../utilities/isSuperAdmin'
 
-export const tenants: Access = ({ req: { user }, data }) => {
-  if (user) {
-    return (
-      // individual documents
-      (data?.tenant?.id && user?.lastLoggedInTenant?.id === data.tenant.id) ||
-      (!user?.lastLoggedInTenant?.id && isSuperAdmin(user)) || {
-        // list of documents
-        tenant: {
-          in: user?.tenants?.map(
-            ({
-              tenant,
-            }: {
-              tenant: {
-                id: string
-              }
-            }) => tenant.id,
-          ),
-        },
-      }
-    )
+export const tenants: Access = ({ req: { user }, data }) =>
+  // individual documents
+  (data?.tenant?.id && user?.lastLoggedInTenant?.id === data.tenant.id) ||
+  (!user?.lastLoggedInTenant?.id && isSuperAdmin(user)) || {
+    // list of documents
+    tenant: {
+      equals: user?.lastLoggedInTenant?.id,
+    },
   }
-
-  return true
-}
