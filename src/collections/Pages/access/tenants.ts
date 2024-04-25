a13587b019c304dbdcc87109b965562a7ba5b383
Rhysypops <developer@meridiantrust.co.uk>
2024-04-25 14:05:16 +0100

    1

--- src/collections/Pages/access/tenants.ts
+++ src/collections/Pages/access/tenants.ts
@@ -1,40 +1,12 @@
 import type { Access } from 'payload/types'
 import { isSuperAdmin } from '../../../utilities/isSuperAdmin'
 
-export const tenants: Access = ({ req, data }) => {
-  // If user is logged in, only show documents that belong to the tenants they have access to
-  if (req.user) {
-    return (
-      // individual documents
-      (data?.id && req.user?.lastLoggedInTenant?.id === data.id) ||
-      (!req.user?.lastLoggedInTenant?.id && isSuperAdmin(req.user)) || {
-        // list of documents
-        id: isSuperAdmin(req.user)
-          ? {
-              exists: true,
-            }
-          : {
-              in: req.user?.tenants?.map(
-                ({
-                  tenant,
-                }: {
-                  tenant: {
-                    id: string
-                  }
-                }) => tenant.id,
-              ),
-            },
-      }
-    )
+export const tenants: Access = ({ req: { user }, data }) =>
+  // individual documents
+  (data?.tenant?.id && user?.lastLoggedInTenant?.id === data.tenant.id) ||
+  (!user?.lastLoggedInTenant?.id && isSuperAdmin(user)) || {
+    // list of documents
+    tenant: {
+      equals: user?.lastLoggedInTenant?.id,
+    },
   }
-
-  // If user is not logged in, only show documents that belong to the tenant that matches the current domain
-  return (
-    (data?.id && data.domain === req.host) || {
-      // list of documents
-      domain: {
-        equals: req.host,
-      },
-    }
-  )
-}
--- src/collections/Tenants/access/tenants.ts
+++ src/collections/Tenants/access/tenants.ts
@@ -1,41 +1,44 @@
 import type { Access } from 'payload/types'
 import { isSuperAdmin } from '../../../utilities/isSuperAdmin'
 
-export const tenants: Access = ({ req, data }) => {
+export const tenants: Access = ({ req: { user }, data }) => {
   console.log(data)
   // If user is logged in, only show documents that belong to the tenants they have access to
-  if (req.user) {
-    return (
-      // individual documents
-      (data?.id && req.user?.lastLoggedInTenant?.id === data.id) ||
-      (!req.user?.lastLoggedInTenant?.id && isSuperAdmin(req.user)) || {
-        // list of documents
-        id: isSuperAdmin(req.user)
-          ? {
-              exists: true,
-            }
-          : {
-              in: req.user?.tenants?.map(
-                ({
-                  tenant,
-                }: {
-                  tenant: {
-                    id: string
-                  }
-                }) => tenant.id,
-              ),
-            },
-      }
-    )
-  }
 
-  // If user is not logged in, only show documents that belong to the tenant that matches the current domain
-  return (
-    (data?.id && data.domain === req.host) || {
-      // list of documents
-      domain: {
-        equals: req.host,
-      },
-    }
-  )
+  return true
+
+  // if (req.user) {
+  //   return (
+  //     // individual documents
+  //     (data?.id && req.user?.lastLoggedInTenant?.id === data.id) ||
+  //     (!req.user?.lastLoggedInTenant?.id && isSuperAdmin(req.user)) || {
+  //       // list of documents
+  //       id: isSuperAdmin(req.user)
+  //         ? {
+  //             exists: true,
+  //           }
+  //         : {
+  //             in: req.user?.tenants?.map(
+  //               ({
+  //                 tenant,
+  //               }: {
+  //                 tenant: {
+  //                   id: string
+  //                 }
+  //               }) => tenant.id,
+  //             ),
+  //           },
+  //     }
+  //   )
+  // }
+
+  // // If user is not logged in, only show documents that belong to the tenant that matches the current domain
+  // return (
+  //   (data?.id && data.domain === req.host) || {
+  //     // list of documents
+  //     domain: {
+  //       equals: req.host,
+  //     },
+  //   }
+  // )
 }
