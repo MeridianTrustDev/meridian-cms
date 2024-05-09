43db56727ec5bca063a42fae7e61cc0403ce8d3e
Rhysypops <developer@meridiantrust.co.uk>
2024-05-09 11:14:24 +0100

    1

--- src/blocks/Columns/index.ts
+++ src/blocks/Columns/index.ts
@@ -1,25 +1,24 @@
-import { Block, Field } from "payload/types";
-import { MediaBlock } from "../MediaBlock";
-import { Text } from "../Content";
+import { Block, Field } from 'payload/types'
+import { MediaBlock } from '../MediaBlock'
+import { Text } from '../Content'
+import { Events } from '../Events'
+import { News } from '../News'
 
 export const Columns: Block = {
-  slug: "columnsBlock",
+  slug: 'columnsBlock',
   fields: [
     {
-      name: "columns",
-      label: "Columns",
-      type: "array",
+      name: 'columns',
+      label: 'Columns',
+      type: 'array',
       fields: [
         {
-          name: "content",
-          type: "blocks",
+          name: 'content',
+          type: 'blocks',
           required: true,
-          blocks: [MediaBlock, Text],
-          admin: {
-            condition: (data) => data.type === "page",
-          },
+          blocks: [Events, MediaBlock, News, Text],
         },
       ],
     },
   ],
-};
+}
