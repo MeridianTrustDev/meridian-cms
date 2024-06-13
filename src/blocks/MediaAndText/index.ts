d1addc14c88c695af03641ee0bd829820f3a3e39
Rhysypops <developer@meridiantrust.co.uk>
2024-06-13 10:47:45 +0100

    Update index.ts

--- src/blocks/MediaAndText/index.ts
+++ src/blocks/MediaAndText/index.ts
@@ -29,5 +29,61 @@ export const MediaAndText: Block = {
       type: 'richText',
       required: true,
     },
+    {
+      name: 'readMore',
+      type: 'group',
+      fields: [
+        {
+          name: 'showReadMore',
+          type: 'checkbox',
+          label: 'Show "Read More" button',
+        },
+        {
+          name: 'readMoreText',
+          type: 'text',
+          admin: {
+            condition: (_, siblingData) => siblingData.showReadMore,
+          },
+        },
+        {
+          name: 'readMoreLink',
+          type: 'group',
+          admin: {
+            condition: (_, siblingData) => siblingData.showReadMore,
+          },
+          fields: [
+            {
+              name: 'target',
+              type: 'select',
+              options: [
+                {
+                  label: 'Page',
+                  value: 'page',
+                },
+                {
+                  label: 'Custom URL',
+                  value: 'custom',
+                },
+              ],
+            },
+            {
+              name: 'page',
+              type: 'relationship',
+              relationTo: 'pages',
+              admin: {
+                condition: (_, siblingData) => siblingData.target === 'page',
+              },
+            },
+            {
+              name: 'url',
+              type: 'text',
+              admin: {
+                condition: (_, siblingData) => siblingData.target === 'custom',
+              },
+            },
+          ],
+        },
+      ],
+    },
   ],
 }
